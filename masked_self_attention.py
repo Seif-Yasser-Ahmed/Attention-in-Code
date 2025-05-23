b383b0f7e6ff8688ac9be66506d5521ceccc87c8
Seif Yasser <21p0102@eng.asu.edu.eg>
2025-05-23 06:33:29 +0300

    Implement MaskedSelfAttention

--- masked_self_attention.py
+++ masked_self_attention.py
@@ -2,10 +2,26 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class MaskedSelfAttention(nn.Module):
-    def __init__():
-        pass
+    def __init__(self, d_model=2, row_dim=0, col_dim=1):
+        super().__init__()
+        self.W_q = nn.Linear(d_model, d_model, bias=False)  # Query
+        self.W_k = nn.Linear(d_model, d_model, bias=False)  # Key
+        self.W_v = nn.Linear(d_model, d_model, bias=False)  # Value
+        self.row_dim = row_dim
+        self.col_dim = col_dim
 
-
-    def forward():
-        pass
+    def forward(self, token_encodings, mask=None):
+        q = self.W_q(token_encodings)
+        k = self.W_k(token_encodings)
+        v = self.W_v(token_encodings)
+        sims = torch.matmul(q, k.transpose(
+            dim0=self.row_dim, dim1=self.col_dim))
+        scaled_sims = sims/torch.tensor(k.size(self.col_dim)**0.5)
+        if mask is not None:
+            scaled_sims = scaled_sims.masked_fill(
+                mask=mask, value=float('-inf'))
+        attention_percents = F.softmax(scaled_sims, dim=self.col_dim)
+        attention_output = torch.matmul(attention_percents, v)
+        return attention_output
